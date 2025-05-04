4e51ee9673e40c2756468470e59544cd8dc77f96
Johnelli Mickel Pordan <jmpordan@gmail.com>
2025-05-04 20:22:20 +0800

    updated portfolio

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ function closeModal() {
     const modal = document.getElementById("imageModal");
     modal.style.display = "none";
 }
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
